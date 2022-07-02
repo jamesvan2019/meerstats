0a982852cfc434064d8b788c6d04a0e3215a6f81
James <James@gmail.com>
2022-07-02 14:17:19 +0800

    fix

--- app.js
+++ app.js
@@ -6,35 +6,29 @@ var http = require('http');
 // Init WS SECRET
 var WS_SECRET;
 
-if( !_.isUndefined(process.env.WS_SECRET) && !_.isNull(process.env.WS_SECRET) )
-{
-	if( process.env.WS_SECRET.indexOf('|') > 0 )
-	{
+if (!_.isUndefined(process.env.WS_SECRET) && !_.isNull(process.env.WS_SECRET)) {
+	if (process.env.WS_SECRET.indexOf('|') > 0) {
 		WS_SECRET = process.env.WS_SECRET.split('|');
 	}
-	else
-	{
+	else {
 		WS_SECRET = [process.env.WS_SECRET];
 	}
 }
-else
-{
+else {
 	try {
 		var tmp_secret_json = require('./ws_secret.json');
 		WS_SECRET = _.values(tmp_secret_json);
 	}
-	catch (e)
-	{
+	catch (e) {
 		console.error("WS_SECRET NOT SET!!!");
 	}
 }
 
-var banned   = require('./lib/utils/config').banned;
+var banned = require('./lib/utils/config').banned;
 var reserved = require('./lib/utils/config').reserved;
 
 // Init http server
-if( process.env.NODE_ENV !== 'production' )
-{
+if (process.env.NODE_ENV !== 'production') {
 	var app = require('./lib/express');
 	server = http.createServer(app);
 }
@@ -82,14 +76,11 @@ external.plugin('emit', require('primus-emit'));
 var Collection = require('./lib/collection');
 var Nodes = new Collection(external);
 
-Nodes.setChartsCallback(function (err, charts)
-{
-	if(err !== null)
-	{
+Nodes.setChartsCallback(function (err, charts) {
+	if (err !== null) {
 		console.error('COL', 'CHR', 'Charts error:', err);
 	}
-	else
-	{
+	else {
 		client.write({
 			action: 'charts',
 			data: charts
@@ -99,38 +90,31 @@ Nodes.setChartsCallback(function (err, charts)
 
 
 // Init API Socket events
-api.on('connection', function (spark)
-{
+api.on('connection', function (spark) {
 	console.info('API', 'CON', 'Open:', spark.address.ip);
 
-	spark.on('hello', function (data)
-	{
+	spark.on('hello', function (data) {
 		console.info('API', 'CON', 'Hello', data['id']);
 
-		if( _.isUndefined(data.secret) || WS_SECRET.indexOf(data.secret) === -1 || banned.indexOf(spark.address.ip) >= 0 || _.isUndefined(data.id) || reserved.indexOf(data.id) >= 0 )
-		{
+		if (_.isUndefined(data.secret) || WS_SECRET.indexOf(data.secret) === -1 || banned.indexOf(spark.address.ip) >= 0 || _.isUndefined(data.id) || reserved.indexOf(data.id) >= 0) {
 			spark.end(undefined, { reconnect: false });
 			console.error('API', 'CON', 'Closed - wrong auth', data);
 
 			return false;
 		}
 
-		if( !_.isUndefined(data.id) && !_.isUndefined(data.info) )
-		{
+		if (!_.isUndefined(data.id) && !_.isUndefined(data.info)) {
 			data.ip = spark.address.ip;
 			data.spark = spark.id;
 			data.latency = spark.latency || 0;
 
-			Nodes.add( data, function (err, info)
-			{
-				if(err !== null)
-				{
+			Nodes.add(data, function (err, info) {
+				if (err !== null) {
 					console.error('API', 'CON', 'Connection error:', err);
 					return false;
 				}
 
-				if(info !== null)
-				{
+				if (info !== null) {
 					spark.emit('ready');
 
 					console.success('API', 'CON', 'Connected', data.id);
@@ -145,20 +129,14 @@ api.on('connection', function (spark)
 	});
 
 
-	spark.on('update', function (data)
-	{
-		if( !_.isUndefined(data.id) && !_.isUndefined(data.stats) )
-		{
-			Nodes.update(data.id, data.stats, function (err, stats)
-			{
-				if(err !== null)
-				{
+	spark.on('update', function (data) {
+		if (!_.isUndefined(data.id) && !_.isUndefined(data.stats)) {
+			Nodes.update(data.id, data.stats, function (err, stats) {
+				if (err !== null) {
 					console.error('API', 'UPD', 'Update error:', err);
 				}
-				else
-				{
-					if(stats !== null)
-					{
+				else {
+					if (stats !== null) {
 						client.write({
 							action: 'update',
 							data: stats
@@ -171,27 +149,20 @@ api.on('connection', function (spark)
 				}
 			});
 		}
-		else
-		{
+		else {
 			console.error('API', 'UPD', 'Update error:', data);
 		}
 	});
 
 
-	spark.on('block', function (data)
-	{
-		if( !_.isUndefined(data.id) && !_.isUndefined(data.block) )
-		{
-			Nodes.addBlock(data.id, data.block, function (err, stats)
-			{
-				if(err !== null)
-				{
+	spark.on('block', function (data) {
+		if (!_.isUndefined(data.id) && !_.isUndefined(data.block)) {
+			Nodes.addBlock(data.id, data.block, function (err, stats) {
+				if (err !== null) {
 					console.error('API', 'BLK', 'Block error:', err);
 				}
-				else
-				{
-					if(stats !== null)
-					{
+				else {
+					if (stats !== null) {
 						client.write({
 							action: 'block',
 							data: stats
@@ -204,25 +175,20 @@ api.on('connection', function (spark)
 				}
 			});
 		}
-		else
-		{
+		else {
 			console.error('API', 'BLK', 'Block error:', data);
 		}
 	});
 
 
-	spark.on('pending', function (data)
-	{
-		if( !_.isUndefined(data.id) && !_.isUndefined(data.stats) )
-		{
+	spark.on('pending', function (data) {
+		if (!_.isUndefined(data.id) && !_.isUndefined(data.stats)) {
 			Nodes.updatePending(data.id, data.stats, function (err, stats) {
-				if(err !== null)
-				{
+				if (err !== null) {
 					console.error('API', 'TXS', 'Pending error:', err);
 				}
 
-				if(stats !== null)
-				{
+				if (stats !== null) {
 					client.write({
 						action: 'pending',
 						data: stats
@@ -232,62 +198,50 @@ api.on('connection', function (spark)
 				}
 			});
 		}
-		else
-		{
+		else {
 			console.error('API', 'TXS', 'Pending error:', data);
 		}
 	});
 
 
-	spark.on('stats', function (data)
-	{
-		if( !_.isUndefined(data.id) && !_.isUndefined(data.stats) )
-		{
+	spark.on('stats', function (data) {
+		if (!_.isUndefined(data.id) && !_.isUndefined(data.stats)) {
 
-			Nodes.updateStats(data.id, data.stats, function (err, stats)
-			{
-				if(err !== null)
-				{
+			Nodes.updateStats(data.id, data.stats, function (err, stats) {
+				if (err !== null) {
 					console.error('API', 'STA', 'Stats error:', err);
 				}
-				else
-				{
-					if(stats !== null)
-					{
+				else {
+					if (stats !== null) {
 						client.write({
 							action: 'stats',
 							data: stats
 						});
 
-						console.success('API', 'STA', 'Stats from:', data.id);
+						console.success('API', 'STA', 'Stats from:', data, data.id);
 					}
 				}
 			});
 		}
-		else
-		{
+		else {
 			console.error('API', 'STA', 'Stats error:', data);
 		}
 	});
 
 
-	spark.on('history', function (data)
-	{
+	spark.on('history', function (data) {
 		console.success('API', 'HIS', 'Got history from:', data.id);
 
 		var time = chalk.reset.cyan((new Date()).toJSON()) + " ";
 		console.time(time, 'COL', 'CHR', 'Got charts in');
 
-		Nodes.addHistory(data.id, data.history, function (err, history)
-		{
+		Nodes.addHistory(data.id, data.history, function (err, history) {
 			console.timeEnd(time, 'COL', 'CHR', 'Got charts in');
 
-			if(err !== null)
-			{
+			if (err !== null) {
 				console.error('COL', 'CHR', 'History error:', err);
 			}
-			else
-			{
+			else {
 				client.write({
 					action: 'charts',
 					data: history
@@ -297,8 +251,7 @@ api.on('connection', function (spark)
 	});
 
 
-	spark.on('node-ping', function (data)
-	{
+	spark.on('node-ping', function (data) {
 		var start = (!_.isUndefined(data) && !_.isUndefined(data.clientTime) ? data.clientTime : null);
 
 		spark.emit('node-pong', {
@@ -310,19 +263,14 @@ api.on('connection', function (spark)
 	});
 
 
-	spark.on('latency', function (data)
-	{
-		if( !_.isUndefined(data.id) )
-		{
-			Nodes.updateLatency(data.id, data.latency, function (err, latency)
-			{
-				if(err !== null)
-				{
+	spark.on('latency', function (data) {
+		if (!_.isUndefined(data.id)) {
+			Nodes.updateLatency(data.id, data.latency, function (err, latency) {
+				if (err !== null) {
 					console.error('API', 'PIN', 'Latency error:', err);
 				}
 
-				if(latency !== null)
-				{
+				if (latency !== null) {
 					// client.write({
 					// 	action: 'latency',
 					// 	data: latency
@@ -332,8 +280,7 @@ api.on('connection', function (spark)
 				}
 			});
 
-			if( Nodes.requiresUpdate(data.id) )
-			{
+			if (Nodes.requiresUpdate(data.id)) {
 				var range = Nodes.getHistory().getHistoryRequestRange();
 
 				spark.emit('history', range);
@@ -345,16 +292,12 @@ api.on('connection', function (spark)
 	});
 
 
-	spark.on('end', function (data)
-	{
-		Nodes.inactive(spark.id, function (err, stats)
-		{
-			if(err !== null)
-			{
+	spark.on('end', function (data) {
+		Nodes.inactive(spark.id, function (err, stats) {
+			if (err !== null) {
 				console.error('API', 'CON', 'Connection end error:', err);
 			}
-			else
-			{
+			else {
 				client.write({
 					action: 'inactive',
 					data: stats
@@ -368,26 +311,22 @@ api.on('connection', function (spark)
 
 
 
-client.on('connection', function (clientSpark)
-{
-	clientSpark.on('ready', function (data)
-	{
+client.on('connection', function (clientSpark) {
+	clientSpark.on('ready', function (data) {
 		clientSpark.emit('init', { nodes: Nodes.all() });
 
 		Nodes.getCharts();
 	});
 
-	clientSpark.on('client-pong', function (data)
-	{
+	clientSpark.on('client-pong', function (data) {
 		var serverTime = _.get(data, "serverTime", 0);
-		var latency = Math.ceil( (_.now() - serverTime) / 2 );
+		var latency = Math.ceil((_.now() - serverTime) / 2);
 
 		clientSpark.emit('client-latency', { latency: latency });
 	});
 });
 
-var latencyTimeout = setInterval( function ()
-{
+var latencyTimeout = setInterval(function () {
 	client.write({
 		action: 'client-ping',
 		data: {
@@ -398,8 +337,7 @@ var latencyTimeout = setInterval( function ()
 
 
 // Cleanup old inactive nodes
-var nodeCleanupTimeout = setInterval( function ()
-{
+var nodeCleanupTimeout = setInterval(function () {
 	client.write({
 		action: 'init',
 		data: Nodes.all()
@@ -407,7 +345,7 @@ var nodeCleanupTimeout = setInterval( function ()
 
 	Nodes.getCharts();
 
-}, 1000*60*60);
+}, 1000 * 60 * 60);
 
 server.listen(process.env.PORT || 3000);
 
